24c88438b2344f5aa64b8e32d25f8fcd4c54b630
Joel Cardinal <jcardinal@cpocommerce.com>
2015-07-04 19:16:27 -0700

    Moved references to separate file, added Help and About sections

--- app.js
+++ app.js
@@ -70,18 +70,18 @@ function updateIframe(){
 	for (k in data){
 		switch( data[k][0] ){
 			case 'html':
-				html = data[k][1] ? data[k][1]: '<div style="font-family:monospace;color:#D1CACA;margin-top:31px;margin">Cycles Editors: Ctrl+j<br>Update Render: Update button or Ctrl+k<br>Resize Workspace: Ctrl+d<br>Resize Render: Ctrl+f</div>';
+				html = data[k][1] ? data[k][1]: '<div style="font-family:monospace;color:#D1CACA;margin-top:31px;margin">Cycle Editors: Ctrl+j<br>Update Render: Update button or Ctrl+k<br>Resize Workspace: Ctrl+d<br>Resize Render: Ctrl+f</div>';
 				break;
 			case 'css':
 				css = data[k][1].split('%C2%A0').join('');
 				break;
-			case 'js':
+			case 'javascript':
 				js = data[k][1];
 				break;
 		}
 	}
 	
-  var page = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0,maximum-scale=1.0,initial-scale=1.0"><title>' + (document.querySelector('.name').value || 'My Website') + '</title><style>' + decodeURIComponent(css) + '</style></head><body>' + decodeURIComponent(html) + '<' + 'script>' + decodeURIComponent(js) + '<'+'/script></body></html>';
+  var page = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0,maximum-scale=1.0,initial-scale=1.0"><title>' + (document.querySelector('.name').value || 'My Website') + '</title><style>' + decodeURIComponent(css) + '</style></head><body>' + decodeURIComponent(html) + '<' + 'script>' + decodeURIComponent(js) + '<' + '/script></body></html>';
 	var iframe = document.querySelector('div.rightColumn iframe');
 
   if (iframe.contentDocument){
@@ -156,6 +156,38 @@ function toggleEye(){
 		elm.classList.remove('darkEye');
 		elm.classList.add('lightEye');
 		document.getElementById('togetherjs-container').style.display = 'block';
+	}
+}
+
+// toggle help dialog
+function toggleHelp(e){
+	var elem = document.getElementById('helpMsg'); 
+	var elemToClose = document.getElementById('aboutMsg');
+	var elemToRemoveStyle = document.getElementById('aboutBtn');
+	if(	getComputedStyle(elem,null).getPropertyValue('display') === 'none'  ){
+		elemToClose.style.display = 'none';
+		elemToRemoveStyle.removeAttribute("style");
+		elem.style.display = 'block';
+		e.target.style.color = '#FFFFFF'
+	}else{
+		elem.style.display = 'none';
+		e.target.removeAttribute("style");
+	}
+}
+
+// toggle help dialog
+function toggleAbout(e){
+	var elem = document.getElementById('aboutMsg');
+	var elemToClose = document.getElementById('helpMsg');
+	var elemToRemoveStyle = document.getElementById('helpBtn');
+	if(	getComputedStyle(elem,null).getPropertyValue('display') === 'none'  ){
+		elemToClose.style.display = 'none';
+		elemToRemoveStyle.removeAttribute("style");
+		elem.style.display = 'block';
+		e.target.style.color = '#FFFFFF'
+	}else{
+		elem.style.display = 'none';
+		e.target.removeAttribute("style");
 	}
 }
 
@@ -247,6 +279,8 @@ document.addEventListener('DOMContentLoaded', function(){
 	// listeners
 	document.querySelector('#updateBtn').addEventListener('click', updateIframe, false );
 	document.querySelector('#downloadBtn').addEventListener('click', download, false );
+	document.querySelector('#helpBtn').addEventListener('click', toggleHelp, false );
+	document.querySelector('#aboutBtn').addEventListener('click', toggleAbout, false );
 	document.querySelector('#collaborateBtn').addEventListener('click', TogetherJS, false );
 	document.querySelector('.eye').addEventListener('click', toggleEye, false );
 });
@@ -254,12 +288,12 @@ document.addEventListener('DOMContentLoaded', function(){
 /*
 
 TODO:
+- get domain .com .rocks .school .site .team .page .education .academy .exchange .website .link .band
+	htmlnoobs.com
 - Add name input for togetherjs
 		var userName = window.prompt('Please enter your name:');
 		TogetherJS.require("peers").Self.name = userName;
 		TogetherJS.require("peers").Self.update(TogetherJS.require("peers").Self);
-- Add Help nav with key commands, togetherjs...etc.
-- Add About with some info about the project
 - Create normalize.css file (test on diff. browsers)?
 - Save content
 	https://github.com/mozilla/localForage
@@ -275,10 +309,5 @@ TODO:
 - Multi-language support
 	https://hacks.mozilla.org/2014/12/introducing-the-javascript-internationalization-api/
 - Login? Need to think about how this will affect user if added later (lose all local data)
-
-Resources:
-http://youmightnotneedjquery.com/
-http://pixlr.com/editor/
-http://duri.me/
 
 */
